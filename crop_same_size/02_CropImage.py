414d8981b46b9d9d7b7ff044733cde0e29971a59
Donghwan, Lee <35002932+stathwan@users.noreply.github.com>
2018-09-14 19:34:30 +0900

    .
    .

--- crop_same_size/02_CropImage.py
+++ crop_same_size/02_CropImage.py
@@ -18,18 +18,24 @@ path_list=glob.glob(img_Dir+'\*')
 
 result = []
 
-for j in range(len(path_list)//2):
 
-    im_path=path_list[1+j*2]
-    img=plt.imread(im_path)
-    with open(path_list[0+j*2],'r') as tsv :
-        bbox = [line.strip().split('\t') for line in tsv]
-
-    for i in range(len(bbox)):
-        x1=int(bbox[i][0])
-        y1=int(bbox[i][1])
-        x2=int(bbox[i][2])
-        y2=int(bbox[i][3])
-        crop_img=img[y1:y2,x1:x2,0:3]
-        plt.imsave(w_Dir+'\crop\{}{}.jpg'.format(im_path.split('\\')[-1][:-4],i),crop_img)
-
+imgFilenames = [f for f in os.listdir(img_Dir) if f.lower().endswith(".jpg")]
+for index,filename in enumerate(imgFilenames):
+    CropInfoPath = os.path.join(img_Dir, filename[:-4] + ".bboxes.tsv")
+    
+    if os.path.exists(CropInfoPath):
+        
+        img=plt.imread(os.path.join(img_Dir, filename))
+        with open(CropInfoPath,'r') as tsv :
+            bbox = [line.strip().split('\t') for line in tsv]
+    
+        try:
+            for i in range(len(bbox)):
+                x1=int(bbox[i][0])
+                y1=int(bbox[i][1])
+                x2=int(bbox[i][2])
+                y2=int(bbox[i][3])
+                crop_img=img[y1:y2,x1:x2,0:3]
+                plt.imsave(w_Dir+'\crop\{}{}.png'.format(filename[:-4],i),crop_img)
+        except:
+            pass
